c66ba830a4a220896accf8917746991baf1bd341
Aishwarya U <aishwaryaofficial27@gmail.com>
2026-05-14 21:32:15 +0530

    Add files via upload

---
+++ List Slicing #5.py
@@ -0,0 +1,23 @@
+# Basic Example
+nums=[10,20,30,40,50]
+print(nums[0:3])
+
+# First N Elements
+print(nums[:4])
+
+# From Index to End
+print(nums[3:])
+
+# Full Copy
+print(nums[:])
+
+# Reverse a list
+print(nums[::-1])
+
+# Alternate Elements
+print(nums[::2])
+
+# Negative Indexing
+print(nums[-4])
+print(nums[-3:])
+
