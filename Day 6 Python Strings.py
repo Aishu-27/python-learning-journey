81845ba997fb586e80dc87a3be68883dc1f3eb62
Aishwarya U <aishwaryaofficial27@gmail.com>
2026-05-17 18:44:21 +0530

    Add files via upload

---
+++ Day 6 Python Strings.py
@@ -0,0 +1,23 @@
+# Creating Strings
+a="Hello"
+b='This is Aishwarya'
+print(a,b)
+
+#Accessing Charaxters
+text="Aishwarya"
+print(text[5])
+print(text[2])
+print(text[7])
+#Negative Indexing
+print(text[-3])
+print(text[-5])
+
+# String Slicing
+word="Python Programming"
+print(word[4:9])
+print(word[::-1])
+print(word[::2])
+
+#Length
+text="Aishwarya Loves Programming"
+print(len(text))
