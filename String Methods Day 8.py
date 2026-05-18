a908d6bb4cb4a296dce694aea9cc760dc482206c
Aishwarya U <aishwaryaofficial27@gmail.com>
2026-05-18 15:32:10 +0530

    Add files via upload

---
+++ String Methods Day 8.py
@@ -0,0 +1,25 @@
+# Upper
+text="python programming"
+print(text.upper())
+
+# lower
+text="I LOVE PROGRAMMING"
+print(text.lower())
+
+# Capitalize
+name="aishwarya"
+print(name.capitalize())
+
+# strip
+text="Hello    World   This   is    Aishwarya  Loves Python    "
+print(text.strip())
+
+# replace
+text="Aishu Loves Dog"
+print(text.replace("Dog", "Cat"))
+
+# split
+fruits="Apple,Orange,Banana"
+print(fruits.split())
+
+
