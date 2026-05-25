0a6fe7d7a3ba85f72d0d5acede1f2b8d1adec81b
Aishwarya U <aishwaryaofficial27@gmail.com>
2026-05-25 15:34:10 +0530

    Updation in Day 12

--- Day 12.py
+++ Day 12.py
@@ -1,13 +1,7 @@
 # Two Sum
-nums=[2,7,11,15]
-target=9
-
-seen={}
-
+nums=[3,2,4]
+target=6
 for i in range(len(nums)):
-    needed = target - nums[i]
-
-    if needed in seen:
-        print([seen[needed], i])
-
-    seen[nums[i]] = i
+    for j in range(i+1,len(nums)):
+        if nums[i] + nums[j] == target:
+            print([i,j])
